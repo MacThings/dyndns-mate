5b85d77d8e54973429d9fd2148166bd941ee406d
MacThings <info@sl-soft.de>
2021-05-29 23:08:35 +0200

    .

---
+++ DynDNS Mate/MainWindow.swift
@@ -0,0 +1,211 @@
+//
+//  ViewController.swift
+//  DynDNS Manager
+//
+//  Created by Prof. Dr. Luigi on 29.05.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    
+    let scriptPath = Bundle.main.path(forResource: "/script/script", ofType: "command")!
+    
+    @IBOutlet weak var daemon_dot: NSImageView!
+    @IBOutlet weak var status_dot: NSImageView!
+    
+    
+    @IBOutlet weak var company_logo: NSImageView!
+    
+    @IBOutlet weak var company_selector: NSPopUpButton!
+    
+    @IBOutlet weak var domain: NSTextField!
+    @IBOutlet weak var username: NSTextField!
+    @IBOutlet weak var password: NSSecureTextField!
+    
+    @IBOutlet weak var daemon_button: NSButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        check_status_init()
+
+        let hostname_init = UserDefaults.standard.string(forKey: "Domain")
+        if hostname_init == nil{
+            UserDefaults.standard.set("", forKey: "Domain")
+        }
+        let username_init = UserDefaults.standard.string(forKey: "Username")
+        if username_init == nil{
+            UserDefaults.standard.set("", forKey: "Username")
+        }
+        let password_init = UserDefaults.standard.string(forKey: "Password")
+        if password_init == nil{
+            UserDefaults.standard.set("", forKey: "Password")
+        }
+        
+        let interval_init = UserDefaults.standard.string(forKey: "Interval")
+        if interval_init == nil{
+            UserDefaults.standard.set("60", forKey: "Interval")
+        }
+        
+        let getcompany = UserDefaults.standard.string(forKey: "Company")
+        if getcompany == "ChangeIP" {
+            self.company_logo.image=NSImage(named: "changeip_logo")
+            self.username.isEnabled = true
+        } else if getcompany == "DuckDNS"{
+            self.company_logo.image=NSImage(named: "duckdns_logo")
+            self.username.isEnabled = false
+            self.username.stringValue = ""
+        } else if getcompany == "DynDns"{
+            self.company_logo.image=NSImage(named: "dyndns_logo")
+            self.username.isEnabled = true
+        } else if getcompany == "EasyDNS"{
+            self.company_logo.image=NSImage(named: "easydns_logo")
+            self.username.isEnabled = true
+        } else if getcompany == "FreeDNS"{
+            self.company_logo.image=NSImage(named: "freedns_logo")
+            self.username.isEnabled = true
+        } else if getcompany == "Hurricane Electric"{
+            self.company_logo.image=NSImage(named: "hurricane_logo")
+            self.username.isEnabled = false
+            self.username.stringValue = ""
+        } else if getcompany == "NoIp"{
+            self.company_logo.image=NSImage(named: "noip_logo")
+            self.username.isEnabled = true
+        } else if getcompany == "Strato"{
+            self.company_logo.image=NSImage(named: "strato_logo")
+            self.username.isEnabled = true
+        }
+       
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    
+    @IBAction func select_company(_ sender: Any) {
+        let username_content = UserDefaults.standard.string(forKey: "Username")
+        self.username.stringValue = username_content!
+        
+        let gettitle = (sender as AnyObject).selectedCell()!.title
+        if gettitle == "ChangeIP" {
+            self.company_logo.image=NSImage(named: "changeip_logo")
+            self.username.isEnabled = true
+        } else if gettitle == "DuckDNS"{
+            self.company_logo.image=NSImage(named: "duckdns_logo")
+            self.username.isEnabled = false
+            self.username.stringValue = ""
+        } else if gettitle == "DynDns"{
+            self.company_logo.image=NSImage(named: "dyndns_logo")
+            self.username.isEnabled = true
+        } else if gettitle == "EasyDNS"{
+            self.company_logo.image=NSImage(named: "easydns_logo")
+            self.username.isEnabled = true
+        } else if gettitle == "FreeDNS"{
+            self.company_logo.image=NSImage(named: "freedns_logo")
+            self.username.isEnabled = true
+        } else if gettitle == "Hurricane Electric"{
+            self.company_logo.image=NSImage(named: "hurricane_logo")
+            self.username.isEnabled = false
+            self.username.stringValue = ""
+        } else if gettitle == "NoIp"{
+            self.company_logo.image=NSImage(named: "noip_logo")
+            self.username.isEnabled = true
+        } else if gettitle == "Strato"{
+            self.company_logo.image=NSImage(named: "strato_logo")
+            self.username.isEnabled = true
+        }
+    }
+    
+    @IBAction func update(_ sender: Any) {
+        let company = UserDefaults.standard.string(forKey: "Company")
+        if company == "ChangeIP" {
+            syncShellExec(path: scriptPath, args: ["changeip"])
+        } else if company == "DuckDNS" {
+            syncShellExec(path: scriptPath, args: ["duckdns"])
+        } else if company == "DynDNS" {
+            syncShellExec(path: scriptPath, args: ["dyndns"])
+        } else if company == "EasyDNS" {
+            syncShellExec(path: scriptPath, args: ["easydns"])
+        } else if company == "FreeDNS" {
+            syncShellExec(path: scriptPath, args: ["freedns"])
+        } else if company == "Hurricane Electric" {
+            syncShellExec(path: scriptPath, args: ["hurricane"])
+        } else if company == "NoIp" {
+            syncShellExec(path: scriptPath, args: ["noip"])
+        } else if company == "Strato" {
+            syncShellExec(path: scriptPath, args: ["strato"])
+        }
+        check_status()
+    }
+    
+    
+    @IBAction func daemon(_ sender: Any) {
+        syncShellExec(path: scriptPath, args: ["check_daemon"])
+        let daemon_check = UserDefaults.standard.bool(forKey: "Daemon")
+        if daemon_check == true {
+            self.daemon_dot.image=NSImage(named: "NSStatusAvailable")
+            self.daemon_button.title = "Uninstall daemon"
+            syncShellExec(path: scriptPath, args: ["uninstall_daemon"])
+        } else {
+            self.daemon_dot.image=NSImage(named: "NSStatusUnavailable")
+            self.daemon_button.title = "Install daemon"
+            syncShellExec(path: scriptPath, args: ["install_daemon"])
+        }
+        
+        check_status_init()
+        //check_status()
+        
+    }
+    
+    func syncShellExec(path: String, args: [String] = []) {
+        let process            = Process()
+        process.launchPath     = "/bin/bash"
+        process.arguments      = [path] + args
+        process.launch()
+        process.waitUntilExit()
+    }
+
+    func check_status_init() {
+        syncShellExec(path: scriptPath, args: ["check_status"])
+        syncShellExec(path: scriptPath, args: ["check_daemon"])
+        
+        let status_check = UserDefaults.standard.bool(forKey: "UpdateOK")
+        if status_check == true {
+            self.status_dot.image=NSImage(named: "NSStatusAvailable")
+        } else {
+            self.status_dot.image=NSImage(named: "NSStatusUnavailable")
+        }
+        let daemon_check = UserDefaults.standard.bool(forKey: "Daemon")
+        if daemon_check == true {
+            self.daemon_dot.image=NSImage(named: "NSStatusAvailable")
+            self.daemon_button.title = "Uninstall daemon"
+        } else {
+            self.daemon_dot.image=NSImage(named: "NSStatusUnavailable")
+            self.daemon_button.title = "Install daemon"
+        }
+    }
+    
+    func check_status() {
+        let status_check = UserDefaults.standard.bool(forKey: "UpdateOK")
+        if status_check == true {
+            self.status_dot.image=NSImage(named: "NSStatusAvailable")
+        } else {
+            self.status_dot.image=NSImage(named: "NSStatusUnavailable")
+                let alert = NSAlert()
+                alert.messageText = NSLocalizedString("Uh Oh! An error has occured.", comment: "")
+                alert.informativeText = NSLocalizedString("There was something wrong by submitting your credentials. Please check and try again.", comment: "")
+                alert.alertStyle = .warning
+                alert.icon = NSImage(named: "AppLogo")
+                let Button = NSLocalizedString("I understand", comment: "")
+                alert.addButton(withTitle: Button)
+                alert.runModal()
+                return
+        }
+    }
+}
+
---
+++ DynDNS Mate/script/script.command
@@ -0,0 +1,203 @@
+#!/bin/bash
+#
+
+ScriptHome=$(echo $HOME)
+ScriptTmpPath="$HOME"/.ddm_temp
+MY_PATH="`dirname \"$0\"`"
+cd "$MY_PATH"
+if [ ! -d "$ScriptTmpPath" ]; then
+    mkdir "$ScriptTmpPath"
+fi
+
+################################################################
+####################### Helper Function ########################
+################################################################
+
+function _helpDefaultRead()
+{
+    VAL=$1
+
+    if [ ! -z "$VAL" ]; then
+    defaults read "${ScriptHome}/Library/Preferences/dyndnsmate.slsoft.de.plist" "$VAL"
+    fi
+}
+
+function _helpDefaultWrite()
+{
+    VAL=$1
+    local VAL1=$2
+
+    if [ ! -z "$VAL" ] || [ ! -z "$VAL1" ]; then
+    defaults write "${ScriptHome}/Library/Preferences/dyndnsmate.slsoft.de.plist" "$VAL" "$VAL1"
+    fi
+}
+
+function _helpDefaultWriteBool()
+{
+    VAL=$1
+    local VAL1=$2
+
+    if [ ! -z "$VAL" ] || [ ! -z "$VAL1" ]; then
+    defaults write "${ScriptHome}/Library/Preferences/dyndnsmate.slsoft.de.plist" "$VAL1" -bool $VAL
+    fi
+}
+
+function _helpDefaultDelete()
+{
+    VAL=$1
+
+    if [ ! -z "$VAL" ]; then
+    defaults delete "${ScriptHome}/Library/Preferences/dyndnsmate.slsoft.de.plist" "$VAL"
+    fi
+}
+
+real_ip=$( curl ipecho.net/plain )
+hostname=$( _helpDefaultRead "Domain" )
+dyndns_ip=$( dig +short $hostname )
+_helpDefaultWrite "IP" "$real_ip"
+
+function set_plist()
+{
+
+     if [[ "$update" == *"good"* ]] || [[ "$update" == *"nochg"* ]] || [[ "$update" == *"not changed"* ]] || \
+        [[ "$update" == *"Updated"* ]] || [[ "$update" == *"OK"* ]]; then
+        _helpDefaultWriteBool YES "UpdateOK"
+        echo "ok"
+        echo "$scheme" > /usr/local/bin/dyndnsmate
+    else
+        _helpDefaultWriteBool NO "UpdateOK"
+        echo "error"
+    fi
+}
+
+function check_status()
+{
+    real_ip=$( curl ipecho.net/plain )
+    hostname=$( _helpDefaultRead "Domain" )
+    dyndns_ip=$( dig +short $hostname )
+    
+    if [[ "$real_ip" = "$dyndns_ip" ]]; then
+        _helpDefaultWriteBool YES "UpdateOK"
+    else
+        _helpDefaultWriteBool NO "UpdateOK"
+    fi
+
+}
+
+function check_daemon()
+{
+    daemon_check=$( launchctl list |grep dyndnsmate )
+    if [[ "$daemon_check" = "" ]]; then
+        _helpDefaultWriteBool NO "Daemon"
+        rm /usr/local/bin/dyndnsmate
+    else
+        _helpDefaultWriteBool YES "Daemon"
+    fi
+}
+
+function install_daemon()
+{
+    interval=$( _helpDefaultRead "Interval" )
+    interval=$(( $interval * 60 ))
+    cp de.slsoft.dyndnsmate.plist "$ScriptHome"/Library/LaunchAgents/.
+    chmod 644 "$ScriptHome"/Library/LaunchAgents/de.slsoft.dyndnsmate.plist
+    ../bin/PlistBuddy -c "Set :StartInterval $interval" "$ScriptHome"/Library/LaunchAgents/de.slsoft.dyndnsmate.plist
+    launchctl load -w "$ScriptHome"/Library/LaunchAgents/de.slsoft.dyndnsmate.plist
+}
+
+function uninstall_daemon()
+{
+    launchctl unload -w "$ScriptHome"/Library/LaunchAgents/de.slsoft.dyndnsmate.plist
+    rm "$ScriptHome"/Library/LaunchAgents/de.slsoft.dyndnsmate.plist
+}
+
+function changeip()
+{
+    hostname=$( _helpDefaultRead "Domain" )
+    username=$( _helpDefaultRead "Username" )
+    password=$( _helpDefaultRead "Password" )
+    update=$( curl https://nic.changeip.com/nic/update&u=$username&p=$password&hostname=$hostname&ip=$real_ip&set=1 )
+    scheme=$( echo "curl https://nic.changeip.com/nic/update&u=$username&p=$password&hostname=$hostname&ip=$real_ip&set=1" )
+    
+    set_plist
+}
+
+function duckdns()
+{
+    hostname=$( _helpDefaultRead "Domain" )
+    password=$( _helpDefaultRead "Password" )
+    update=$( curl https://www.duckdns.org/update?domains=$hostname&token=$password&ip=$real_ip )
+    scheme=$( echo "curl https://www.duckdns.org/update?domains=$hostname&token=$password&ip=$real_ip" )
+    
+    set_plist
+}
+
+function dyndns()
+{
+    hostname=$( _helpDefaultRead "Domain" )
+    username=$( _helpDefaultRead "Username" )
+    password=$( _helpDefaultRead "Password" )
+    update=$( curl -4 -s "https://$username:$password@members.dyndns.org/nic/update?hostname=$hostname&myip=$real_ip" )
+    scheme=$( echo "curl -4 -s \"https://$username:$password@members.dyndns.org/nic/update?hostname=$hostname&myip=$real_ip\"" )
+    
+    set_plist
+}
+
+function easydns()
+{
+    hostname=$( _helpDefaultRead "Domain" )
+    username=$( _helpDefaultRead "Username" )
+    password=$( _helpDefaultRead "Password" )
+    update=$( curl --silent --user "$username:$password" https://members.easydns.com/dyn/dyndns.php?wildcard=off&hostname=$hostname&myip=$real_ip )
+    scheme=$( echo "curl --silent --user \"$username:$password\" https://members.easydns.com/dyn/dyndns.php?wildcard=off&hostname=$hostname&myip=$real_ip" )
+    
+    set_plist
+}
+
+function freedns()
+{
+    hostname=$( _helpDefaultRead "Domain" )
+    username=$( _helpDefaultRead "Username" )
+    password=$( _helpDefaultRead "Password" )
+    update=$( curl https://$username:$password@freedns.afraid.org/nic/update?hostname=$hostname&myip=$real_ip )
+    scheme=$( echo "https://$username:$password@freedns.afraid.org/nic/update?hostname=$hostname&myip=$real_ip" )
+    
+    set_plist
+}
+
+function hurricane()
+{
+    hostname=$( _helpDefaultRead "Domain" )
+    password=$( _helpDefaultRead "Password" )
+    update=$( curl https://dyn.dns.he.net/nic/update?hostname=$hostname&password=$password&myip=$real_ip )
+    scheme=$( echo "https://dyn.dns.he.net/nic/update?hostname=$hostname&password=$password&myip=$real_ip" )
+    
+    set_plist
+}
+
+function noip()
+{
+    hostname=$( _helpDefaultRead "Domain" )
+    username=$( _helpDefaultRead "Username" )
+    password=$( _helpDefaultRead "Password" )
+    update=$( curl -s -k -u $username:$password "https://dynupdate.no-ip.com/nic/update?hostname=$hostname&myip=$real_ip" )
+    scheme=$( echo "curl -s -k -u $username:$password \"https://dynupdate.no-ip.com/nic/update?hostname=$hostname&myip=$real_ip\"" )
+    
+    set_plist
+}
+
+function strato()
+{
+    hostname=$( _helpDefaultRead "Domain" )
+    username=$( _helpDefaultRead "Username" )
+    password=$( _helpDefaultRead "Password" )
+    update=$( curl --silent --show-error --insecure --user $username:$password https://dyndns.strato.com/nic/update?hostname=&hostname )
+    scheme=$( echo "curl --silent --show-error --insecure --user $username:$password https://dyndns.strato.com/nic/update?hostname=&hostname" )
+    
+    set_plist
+}
+
+$1
+exit 0
+
+
