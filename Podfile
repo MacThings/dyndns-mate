80b11d0c93c1379642d2ece2529dec7eb83b4ec1
MacThings <info@sl-soft.de>
2021-05-29 23:23:41 +0200

    .

---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+use_frameworks!
+platform :osx, '10.10'
+
+target 'DynDNS Mate' do
+  pod 'Sparkle'
+end
+
---
+++ Pods/Target Support Files/Pods-DynDNS Mate/Pods-DynDNS Mate-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DynDNS_Mate : NSObject
+@end
+@implementation PodsDummy_Pods_DynDNS_Mate
+@end
